86e03ca0ebd1e46fc9400aba482e9777cf17915c
Ivan Robles <irae45@gmail.com>
2026-05-11 18:44:09 -0600

    add ESlint airbnb rules, update dependencies.

--- frontend/__tests__/App.test.tsx
+++ frontend/__tests__/App.test.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import React from "react";
 
-import App from '../src/App';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-test('App component render', async () => {
+import App from "../src/App";
+
+test("App component render", async () => {
   render(<App />);
 
-  expect(screen.getByRole('button')).toHaveTextContent('count');
+  expect(screen.getByRole("button")).toHaveTextContent("count");
 });
